4e6f590528559d29af1e5198de13db89cd11b561
YIWUKUN <yiwukun@sina.com>
2018-03-28 17:34:45 +0800

    Update something

--- 0_pre/LogisticRegression_Iris.py
+++ 0_pre/LogisticRegression_Iris.py
@@ -8,15 +8,15 @@ from matplotlib.colors import ListedColormap
 def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
     # setup marker generator and color map
     markers = ('s', 'x', 'o', '^', 'v')
-    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    colors = ('red', 'blue', 'lightgreen', 'black', 'cyan')
     cmap = ListedColormap(colors[:len(np.unique(y))])
     # plot the decision surface
     x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
-    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-    Z = Z.reshape(xx1.shape)
-    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)  #Z为1-D矩阵
+    Z = Z.reshape(xx1.shape)                                        #Z转为 m*n 矩阵
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
     plt.xlim(xx1.min(), xx1.max())
     plt.ylim(xx2.min(), xx2.max())
     # plot class samples
@@ -25,7 +25,7 @@ def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
     # highlight test samples
     if test_idx:
         X_test, y_test = X[test_idx, :], y[test_idx]
-    plt.scatter(X_test[:, 0], X_test[:, 1], c='', alpha=1.0, linewidth=1, marker='o', s=55, label='test set')
+    plt.scatter(X_test[:, 0], X_test[:, 1], alpha=1.0, marker='v', s=55, label='test set')
 
 
 iris = datasets.load_iris()
@@ -46,7 +46,7 @@ from sklearn.linear_model import LogisticRegression
 lr = LogisticRegression(C=1000.0, random_state=0)
 lr.fit(X_train_std, y_train)
 lr.predict_proba(X_test_std[0,:].reshape(1, -1)) # 查看第一个测试样本属于各个类别的概率
-plot_decision_regions(X_combined_std, y_combined, classifier=lr, test_idx=range(105,150))
+plot_decision_regions(X_combined_std, y_combined, classifier=lr, test_idx=range(105,140))
 plt.xlabel('petal length [standardized]')
 plt.ylabel('petal width [standardized]')
 plt.legend(loc='upper left')
